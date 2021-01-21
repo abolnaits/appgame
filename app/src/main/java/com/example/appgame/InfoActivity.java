e5313bfa5da03d6217aa88ea47ce603e67d52820
Andres Benitez <abolnaits@hotmail.com>
2021-01-21 18:59:42 -0500

    Activity Info
    Info activity

---
+++ app/src/main/java/com/example/appgame/InfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.appgame;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InfoActivity extends Activity {
+
+    //Archivo Main
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.info);
+    }
+}
--- app/src/main/java/com/example/appgame/MainActivity.java
+++ app/src/main/java/com/example/appgame/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.appgame;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void ejecutar_info(View vista){
+        //Creo el intento para lanzar la actividad
+        Intent intento = new Intent(this,InfoActivity.class);
+        //Inicio la actividad
+        startActivity(intento);
+    }
+
 }
